79d4d7bd68f1ec308ca83a48d2bcc569219d9aad
= <=>
2023-12-27 10:10:46 +0100

    first commit

---
+++ lib/features/home/presentation/view/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:crow/crow.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_structure/features/home/presentation/view_models/home_view_model.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends Screen<HomeViewModel> {
+  HomeScreen({super.key});
+
+  @override
+  Widget builder() {
+    return viewModel.obx(
+      (_) => !viewModel.isConnected.value
+          ? Container()
+          : WillPopScope(
+              onWillPop: () async => false,
+              child: Scaffold(
+                backgroundColor: Colors.white,
+                body: ListView(
+                  children: <Widget>[
+                    SizedBox(
+                      height: Get.height * 0.3,
+                    ),
+                    const Center(child: Text('Home')),
+                    SizedBox(
+                      height: Get.height * 0.3,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+    );
+  }
+}
